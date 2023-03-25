58208babc081393ed76061758ecd2ac14e9f2405
nadounegi <it22083027@tsb-yyg.ac.jp>
2023-03-25 17:03:17 +0900

    更新更新

--- js/index.js
+++
@@ -1,8 +0,0 @@
-const header = document.querySelector("header");
-
-/*Sticky Navbar */
-function stickyNavbar() {
-  header.classList.toggle("scrolled", window.pageYOffset > 0);
-}
-
-window.addEventListener("scroll", stickyNavbar);
--- js/test.js
+++ js/test.js
@@ -1,5 +1,5 @@
 const header = document.querySelector("header");
-
+const toggle_btn = document.querySelector(".toggle-btn");
 /*-----------Sticky Navbar--------------- */
 function stickyNavbar() {
   header.classList.toggle("scrolled", window.pageYOffset > 0);
@@ -18,3 +18,15 @@ let sr = ScrollReveal({
 
 sr.reveal(".showcase-info", { delay: 600 });
 sr.reveal(".showcase-image", { origin: "top", delay: 700 });
+
+/*--------------Change Page Theme--------------- */
+function chageTheme() {
+  if (!document.body.classList.contains("dark")) {
+    document.body.classList.add("dark");
+    toggle_btn.classList.replace("uil-moon", "uil-sun");
+  }
+}
+
+toggle_btn.addEventListener("click", () => {
+  chageTheme();
+});
